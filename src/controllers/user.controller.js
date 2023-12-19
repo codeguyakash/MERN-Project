aad227e40d3d0ba24aba5bfd9c059ff1c541c451
Akash <codeguyakash@gmail.com>
2023-12-19 23:49:46 +0530

    add changepassword controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -140,5 +140,17 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         throw new ApiError(401, error?.message || "Invalid Refresh Token")
     }
 })
+const changeCurrentUserPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+    if (!isPasswordCorrect) {
+        throw new ApiError(400, "Incorrect password")
+    }
+    user.password = newPassword;
+    user.save({ validateBeforeSave: false })
+    return res.status(200)
+        .json(new ApiResponse(200, {}, "Password Changed Success"))
+})
 
 export { registerUser, loginUser, logoutUser, refreshAccessToken }
